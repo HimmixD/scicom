d08fb2019a0b70c8f39c0b34a6882367d0cc8d8e
Himmi <himmelias@gmail.com>
2026-03-17 19:11:06 +0100

    finished lu_solve, could be optimized to be faster

--- ES01/ES01.py
+++ ES01/ES01.py
@@ -13,6 +13,12 @@ def create_problem(n):
     return (L, b)
 
 
+def create_problem_full(n):
+    A = np.random.rand(n, n)
+    b = np.random.rand(n, 1)
+    return (A, b)
+
+
 def tri_solve(L, b):
     x = np.empty((len(b), 1))
     for i in range(len(x)):
@@ -24,13 +30,12 @@ def tri_solve_upper(R, z):
     n = len(z)
     x = np.empty((n, 1))
     for i in reversed(range(n)):
-        x[i] = (z[i] - sum(x[j] * R[i, j] for j in range(i+1, n))) / R[i, i]
+        x[i] = (z[i] - sum(x[j] * R[i, j] for j in range(i, n))) / R[i, i]
     return x
 
 
 L = np.array([[3, 0, 0], [1, 5, 0], [2, 3, 1]])
 b = np.array([[3], [2], [1]])
-M = np.array([[3, 2, 5], [7, 5, 0], [2, 8, 9]])
 
 
 def measure_time(func, *args):
@@ -68,7 +73,8 @@ def plot_times():
     plt.legend()
     plt.show()
 
-A = np.array([[10**(-17), 1],[2, 1]])
+
+A = np.array([[10 ** (-17), 1], [2, 1]])
 b = np.array([[1], [3]])
 
 
@@ -77,8 +83,8 @@ def lu_solve(A, b, pv):
     n, n = M.shape
     L = np.zeros((n, n))
     R = np.zeros((n, n))
+    b = b[z]
     for i in range(n):
-        b[i] = b[z[i]]
         for j in range(n):
             if i > j:
                 L[i, j] = M[i, j]
@@ -88,14 +94,41 @@ def lu_solve(A, b, pv):
             else:
                 R[i, j] = M[i, j]
     z_sol = tri_solve(L, b)
-    x = tri_solve_upper(R, z_sol)#
-    x[i] = x[z[i]]
+    x = tri_solve_upper(R, z_sol)
+    x = x[z]
     return x
 
-B = np.array([[1, 2, 3], 
-              [0, 1, 4], 
-              [0, 0, 1]])
-u = np.array([[6], [5], [1]])
 
-#print(tri_solve_upper(B, u))
-print(lu_solve(A, b, True))
+def test_lu_solve(n):
+    A, b = create_problem_full(n)
+    x_own = lu_solve(A, b, False)
+    x_np = np.linalg.solve(A, b)
+    print("Own solution:\n", x_own)
+    print("Numpy solution:\n", x_np)
+    print(np.allclose(x_own, x_np))
+
+
+def plot_times_2():
+    n = [5, 10, 20, 50, 100, 200, 400]
+    time_own = np.zeros(len(n))
+    time_own2 = np.zeros(len(n))
+    time_np = np.zeros(len(n))
+    for i, x in enumerate(n):
+        A, b = create_problem_full(x)
+
+        time_own[i] = measure_time(lu_solve, A, b, False)
+
+        time_np[i] = measure_time(np.linalg.solve, A, b)
+
+    plt.plot(n, time_own, label="lu_solve")
+    plt.plot(n, time_np, label="linalg.solve")
+    plt.xlabel("Mat. dim. n")
+    plt.ylabel("Comput. time (s)")
+    plt.legend()
+    plt.show()
+
+
+B = np.array([[1, 2, 3], [3, 1, 4], [6, 9, 1]])
+u = np.array([[6], [8], [16]])
+
+plot_times_2()
