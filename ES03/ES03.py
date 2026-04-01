80fcc55632181beece3389e4a00cf6c06ab00bad
Himmi <himmelias@gmail.com>
2026-04-01 12:03:23 +0200

    finished both exercises, both work properly

--- ES03/ES03.py
+++ ES03/ES03.py
@@ -2,8 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from qr_decomp import qr_hh
-
-help(qr_hh)
+import random as rd
 
 
 # %% functions
@@ -20,20 +19,35 @@ def pol_fit(x, y, n):
 def qr_solve(A, r, b):
     m, n = A.shape
     Q_b = b.copy()
-    for k in range(n):
+    for k in range(
+        n
+    ):  # loop over all columns of A to apply the Householder transformations to b
         v_k = np.zeros(m)
-        v_k[k] = 1
-        v_k[k + 1 : m] = A[k + 1 : m, k]
+        v_k[k:m] = A[k:m, k]
         pj = 2 * np.dot(v_k, Q_b) / np.dot(v_k, v_k) * v_k
         Q_b -= pj
-    print("Q^T b after applying Q^T:", Q_b)
-    x = np.empty((n, 1))
-    for i in reversed(range(n)):
-        x[i] = (
-            Q_b[i] - sum(x[j] * r[i] + x[j] * A[i, j] for j in range(i + 1, n))
-        ) / r[i]
-    print(x)
-    # funkt nicht, muss noch checken warum
+
+    x = np.zeros((n, 1))
+    R = np.empty((n, n))
+    for i in range(
+        n
+    ):  # construct R from the upper triangular part of A and the diagonal elements from r
+        R[i, i] = r[i]
+        R[i, i + 1 : n] = A[i, i + 1 : n]
+    for i in reversed(range(n)):  # back substitution to solve R x = Q_b
+        x[i] = (Q_b[i] - sum(x[j] * R[i, j] for j in range(i + 1, n))) / R[i, i]
+
+    return x
+
+
+def create_problem():
+    n = rd.randint(1, 100)
+    m = rd.randint(n, n + 10)
+    A = np.random.rand(m, n)
+    x_true = np.random.rand(n)
+    b = A @ x_true
+
+    return A, b, x_true, m, n
 
 
 # %% 5. plot with x values from the data
@@ -67,12 +81,37 @@ This is known as Runge's phenomenon. The value of p(5) also changes significantl
 which indicates that the polynomial is not stable and can give very different results for small changes in the input data."""
 
 
-# %% 6.
+# %% 6. test for randomly generated A and b
+A, b, x_true, m, n = create_problem()
+A_qr, r = qr_hh(A)
+x_qr = qr_solve(A_qr, r, b).transpose()
+lstsq_solution = np.linalg.lstsq(A, b)[0]
+# print("Least squares solution:", lstsq_solution)
+# print("True solution:", x_true)
+# print("QR solution:", x_qr)
+
+print(f"Tested for randomly generated A and b with m = {m} and n = {n}")
+if np.allclose(x_qr, x_true):
+    print("The QR solution is close to the true solution.")
+else:
+    print("The QR solution is not close to the true solution.")
+
+
+# %% 6. test qr_solve with the given A and b
 A = np.array([[1, -1], [10 ** (-8), 0], [0, 10 ** (-8)]])
 b = np.array([0, 0, 10 ** (-6)])
 A_qr, r = qr_hh(A)
 qr_solve(A_qr, r, b)
 # expected output for x is (50,50)
 
-
-# %%
+# %% 6. extra: solving least squares problem from ex05.dat
+Z = np.loadtxt("ex05.dat")
+x = Z[:, 0]
+y = Z[:, 1]
+m = len(x)
+n = 6
+M = np.array([[x_i ** (j - 1) for j in range(1, n + 1)] for x_i in x])
+A_qr, r = qr_hh(M)
+coeffs_qr = qr_solve(A_qr, r, y)
+print("Coefficients from QR decomposition:", coeffs_qr)
+print("Coefficients from pol_fit function:", pol_fit(x, y, n)[0])
